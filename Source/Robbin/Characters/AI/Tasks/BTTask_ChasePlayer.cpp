2a35bae58ed31b12b29930204a08a2fbf1a9150e
Pablo <pablomata999@gmail.com>
2024-05-07 19:11:15 +0200

    ImproveChasing

---
+++ Source/Robbin/Characters/AI/Tasks/BTTask_ChasePlayer.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_ChasePlayer.h"
+#include "Robbin/Characters/NPCs/NPCsAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+
+UBTTask_ChasePlayer::UBTTask_ChasePlayer(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = TEXT("Chasing Player");
+}
+
+EBTNodeResult::Type UBTTask_ChasePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComponent, uint8* NodeMemory)
+{
+	if (auto* const npc = Cast<ANPCsAIController>(OwnerComponent.GetAIOwner()))
+	{
+		auto const PlayerLocation = OwnerComponent.GetBlackboardComponent()->GetValueAsVector(GetSelectedBlackboardKey());
+
+		UAIBlueprintHelperLibrary::SimpleMoveToLocation(npc, PlayerLocation);
+
+		FinishLatentTask(OwnerComponent, EBTNodeResult::Succeeded);
+		return EBTNodeResult::Succeeded;
+	}
+
+	return EBTNodeResult::Failed;
+}
